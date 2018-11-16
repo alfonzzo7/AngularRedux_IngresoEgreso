38882926f69fe0c7c2c9d9e961a1dcc6e14ecc78
Pedro Herrera <alfonzzo7@gmail.com>
2018-11-16 13:13:23 +0100

    Config

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,9 +1,10 @@
 export const firebaseMessages = new Map<string, string>([
+    // Registro
     [ 'auth/email-already-in-use', 'El email ya esta asignado a un usuario' ],
     [ 'auth/invalid-email', 'El email es invalido' ],
     [ 'auth/operation-not-allowed', 'Actualmente no se puede crear usuarios con email y contraseña' ],
     [ 'auth/weak-password', 'La contraseña no cumple los requisitos minimos de validez (al menos 6 caracteres)' ],
-
+    // Login
     [ 'auth/invalid-email', 'El email no es valido' ],
     [ 'auth/user-disabled', 'El usuario esta bloqueado, contacte con el administrador' ],
     [ 'auth/user-not-found', 'El email o la contraseña no son correctos' ],
